2340d5323050d1e94686c13ddbc7f239482308fc
Parav Nagarsheth <parav.nagarsheth@gmail.com>
2023-12-06 00:25:54 +0530

    Initial Commit

---
+++ whisper-sarama/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whisper-sarama
+//
+//  Created by Parav Nagarsheth on 12/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whisper-sarama/whisper_saramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisper_saramaApp.swift
+//  whisper-sarama
+//
+//  Created by Parav Nagarsheth on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct whisper_saramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
